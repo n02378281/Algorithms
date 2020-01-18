6824c32283578c1bddbf263cf57086d623ee49c4
n02378281 <vbc.tkr@gmail.com>
2020-01-18 01:07:24 -0800

    Added new programs..

---
+++ Prep/IsPalindrome.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPrep.Prep
+{
+    class IsPalindrome
+    {
+        public bool IsPalindrome1(string s)
+        {
+            int left = 0;
+            int right = s.Length - 1;
+
+            while (left < right)
+            {
+                bool isLeft = IsAlphaNumeric(s[left]);
+                bool isRight = IsAlphaNumeric(s[right]);
+
+                if (isLeft && isRight)
+                {
+                    if (Char.ToLower(s[left]) != Char.ToLower(s[right]))
+                        return false;
+                    right--;
+                    left++;
+                }
+                if (!isRight)
+                {
+                    right--;
+                }
+                if (!isLeft)
+                {
+                    left++;
+                }
+            }
+
+            return true;
+        }
+
+        private bool IsAlphaNumeric(char v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
