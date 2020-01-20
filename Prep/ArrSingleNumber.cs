7c897700cec761ebc1a8f291e2eab91170f4e172
n02378281 <vbc.tkr@gmail.com>
2020-01-20 02:29:22 -0800

    Added New Programs and changed class names based on topics.

---
+++ Prep/ArrSingleNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPrep.Prep
+{
+    class ArrSingleNumber
+    {
+        //Using XOR but can be easily solved using Stack or Deictionary
+        public int SingleNumber(int[] nums)
+        {
+            if (nums == null)
+                return -1;
+
+            int sum = 0;
+            foreach (var item in nums)
+            {
+                sum ^= item;
+            }
+
+            return sum;
+        }
+        //Using Stack
+    }
+}
---
+++ Prep/BSTInOrderTraversal.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPrep.Prep
+{
+    public class TreeNode
+    {
+        public int value;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int data)
+        {
+            this.value = data;
+            left = null;
+            right = null;
+        }
+    }
+    //    Given a binary tree, return the inorder traversal of its nodes' values.
+
+    //Example:
+
+    //Input: [1,null,2,3]
+    //   1
+    //    \
+    //     2
+    //    /
+    //   3
+
+    //Output: [1,3,2]
+    //    Follow up: Recursive solution is trivial, could you do it iteratively?
+    class BSTInOrderTraversal
+    {
+        public List<Int32> inorderTraversal(TreeNode root)
+        {
+            List<Int32> list = new List<Int32>();
+
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            TreeNode cur = root;
+
+            while (cur != null || stack.Count != 0)
+            {
+                while (cur != null)
+                {
+                    stack.Push(cur);
+                    cur = cur.left;
+                }
+                cur = stack.Pop();
+                list.Add(cur.value);
+                cur = cur.right;
+            }
+
+            return list;
+        }
+
+        public List<Int32> inorderTraversal1(TreeNode root)
+        {
+            List<Int32> list = new List<Int32>();
+            if (root == null) return list;
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            while (root != null || stack.Count != 0)
+            {
+                while (root != null)
+                {
+                    stack.Push(root);
+                    root = root.left;
+                }
+                root = stack.Pop();
+                list.Add(root.value);
+                root = root.right;
+
+            }
+            return list;
+        }
+    }
+}
